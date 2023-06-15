a275ea42a52f753794895703e4ea561063fe0fce
Miguel Acevedo <581miguelacevedo@gmail.com>
2023-06-15 00:31:34 -0400

    removed styles.css. installed sass and added styles.scss.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Applications/MAMP/htdocs/Portfolio/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
